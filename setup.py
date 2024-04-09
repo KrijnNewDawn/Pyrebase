87cba37e2f817e4382db80f64caacc599d83e7f6
KrijnNewDawn <kvandenberg@newdawnrobotics.nl>
2024-04-09 11:56:29 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Pyrebase4',
-    version='4.7.1',
+    version='4.7.2',
     url='https://github.com/nhorvath/Pyrebase4',
     description='A simple python wrapper for the Firebase API with current deps',
     author='nhorvath',
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'requests-toolbelt>=0.7.1,<1.0',
-        'requests>=2.19.1,<2.30',
+        'requests>=2.19.1,<=2.31.0',
         'urllib3>=1.21.1,<2',
         'gcloud>=0.18.3',
         'oauth2client>=4.1.2',
